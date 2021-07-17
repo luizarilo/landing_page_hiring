bc8b987133ad1bf1383ad25fcc588024f3060621
luizarilo <luiz.arilo@gmail.com>
2021-07-17 19:30:07 -0300

    Problema do localStorage resolvido

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,7 @@
-/* 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
- */
-
-$(function(){
-    var tbClientes = localStorage.getItem("tbClientes");
-    tbClientes = JSON.parse(tbClientes); 
-    if(tbClientes == null)
-    tbClientes = [];
-});
-
-function Adicionar(){
-    var cliente = JSON.stringify({
-        Nome     : $("name").val(),
-        Email    : $("email").val()
-    });
-    tbClientes.push(cliente);
-	localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
-	alert("Registro adicionado.");
-	return true;
-}
-
-$("#contactForm").on("submit",function(){
-			return Adicionar();
-}); 
+submitButton.addEventListener("click",function(){
+    let nomeUsuario = document.getElementById("name").value;
+    let emailUsuario = document.getElementById("email").value;
+    localStorage.setItem('nomeUsuario',JSON.stringify(nomeUsuario));
+    localStorage.setItem('emailUsuario',JSON.stringify(emailUsuario))  
+})
+  
